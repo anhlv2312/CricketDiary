bba25063a010da1fac91e2c69ef74505283cec61
Vu Anh Le <anhlv@ymail.com>
2018-04-26 18:50:37 +1000

    new menu

--- js/effect.js
+++ js/effect.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-	$('nav ul li').hide();
-	$('nav ul li.active').show();
-});
+// $(document).ready(function(){
+// 	$('nav ul li:not(.active').hide();
+// });
 
-$(document).on('click', 'nav ul li.active', function(){
-	$('nav ul li').toggle();
-	$('nav ul li.active').show();
-});
+// $(document).on('click', 'nav ul li.active', function(){
+// 	$('nav ul li:not(.active)').toggle();
+// });
