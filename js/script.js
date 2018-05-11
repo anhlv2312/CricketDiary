5dce7768f6e18d4fa9be1b6b030acaecbb515ec0
Vu Anh Le <anhlv@ymail.com>
2018-05-11 10:03:56 +1000

    gallery for mobile done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
+	
 	if ($(window).width() < 750) {
-		$("#trigger-icon").show();
-		$("#main-menu ul").hide();
-		$("#main-menu h2").show();
-		$("#main-menu li.active").hide();
-		$("#trigger-icon").click(function(event){
-			$("#main-menu ul").toggle();
+		$('#trigger-icon').show();
+		$('#main-menu ul').hide();
+		$('#main-menu h2').show();
+		$('#main-menu li.active').hide();
+		$('#trigger-icon').click(function(event){
+			$('#main-menu ul').toggle();
 		});
 	}
+	
+	$('#slideshow ul li').hide();
+	$('#slideshow ul li:first-child').show();
+	$('#slideshow a').show();
+
+	$('#slideshow a.next').click(function(event){
+		$('#slideshow ul li').hide();
+		$('#slideshow ul li:first-child').appendTo($('#slideshow ul'));
+		$('#slideshow ul li:first-child').show();
+	});
+	
+	$('#slideshow a.prev').click(function(event){
+		$('#slideshow ul li').hide();
+		$('#slideshow ul li:last-child').prependTo($('#slideshow ul'));
+		$('#slideshow ul li:last-child').show();
+	});
+
+
 });
