8bf50387bf8c3cf5c78537593b9113d84e7501a0
Vu Anh Le <anhlv@ymail.com>
2018-05-12 18:55:21 +1000

    slideshow ok

--- js/script.js
+++ js/script.js
@@ -1,47 +1,40 @@
 $(document).ready(function() {
-	
-	if ($(window).width() < 750) {
-		$('#main-menu h2').show();
-		$('#main-menu ul').hide();
-		$('#main-menu li.active').hide();
 
-		$('#trigger-icon').show();
-		$('#trigger-icon').click(function(event){
-			$('#main-menu ul').toggle();
-		});	
-	} else {
-		$('#float-list').show();
-		$('#static-list').hide();
-		$('#float-list li p').hide();
-	}
+	$('body').addClass('js');
+
+	$('#main-menu ul').addClass('hide');
+	$('#main-menu ul li.active').addClass('hide');
 	
-	$('#slideshow ul li').hide();
-	$('#slideshow ul li:first-child').show();
-	$('#slideshow a').show();
+	$('#trigger-bar').addClass('enabled');
+	$('#trigger-icon').click(function(event){
+		$('#main-menu ul').toggleClass('hide');
+	});
+
+	$('#worldmap ul li h2').mouseenter(function(event){
+		$(this).addClass('active');
+		$(this).parent().children("p").addClass('active');
+	});
+
+	$('#worldmap ul li h2').mouseleave(function(event){
+		$(this).removeClass('active');
+		$(this).parent().children("p").removeClass('active');
+
+	});
+	
+
+	$('#slideshow').addClass('enabled');
 
 	$('#slideshow a.next').click(function(event){
-		$('#slideshow ul li').hide();
 		$('#slideshow ul li:first-child').appendTo($('#slideshow ul'));
-		$('#slideshow ul li:first-child').show();
 	});
 	
 	$('#slideshow a.prev').click(function(event){
-		$('#slideshow ul li').hide();
 		$('#slideshow ul li:last-child').prependTo($('#slideshow ul'));
-		$('#slideshow ul li:last-child').show();
 	});
 
-	$('#float-list li h2').mouseenter(function(event){
-		$(this).addClass('stop-animation');
-		$(this).parent().children("p").show(); 
-		$(this).parent().children("p").addClass('active');
-		$(this).addClass('stop-animation');
-	});
-	$('#float-list li h2').mouseleave(function(event){
-		$(this).removeClass('stop-animation');
-		$(this).parent().children("p").removeClass('active');
-		$(this).parent().children("p").hide();
-
+	$('button#submit').click(function(event){
+		alert("Sorry, this function hasn't been implemented!");
+		event.preventDefault();
 	});
 
 });
